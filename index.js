095cc1112ab8f6f8b78584e7ea1a694680c9ca4a
rehakmatej <rehakmatej@seznam.cz>
2019-01-28 23:04:24 +0100

    mybe repaired app crash on heroku

--- index.js
+++ index.js
@@ -104,8 +104,8 @@ app.post('/api/authenticate', function (req, res) {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  //res.sendFile(path.join(__dirname + '/client/build/index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
